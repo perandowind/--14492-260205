2adca2175781606d8d0a03bce177bca566d7e8e9
perandowind <perandowind@gmail.com>
2026-02-06 10:38:58 +0900

    6 -r

--- src/test/java/TestUtilTest.java
+++ src/test/java/TestUtilTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,21 +9,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TestUtilTest {
 
     @Test
-    @DisplayName("TestUtil.genScanner()")
+    @DisplayName("TestUtil.setOutByteArray()")
     void t1() {
-        Scanner scanner = TestUtil.getScanner("""
-                등록
-                현재를 사랑하라.
-                작자미상
-                """);
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutByteArray();
 
-        String cmd = scanner.nextLine();
-        String content = scanner.nextLine();
-        String author = scanner.nextLine();
+        System.out.println("안녕하세요");
 
-        assertThat(cmd).isEqualTo("등록");
-        assertThat(content).isEqualTo("현재를 사랑하라.");
-        assertThat(author).isEqualTo("작자미상");
+        String rst = byteArrayOutputStream.toString();
+
+        assertThat(rst).isEqualTo("안녕하세요");
     }
 
 
