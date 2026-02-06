c45769e8f7a08ed1193f9c15c8a41dfea89600c9
perandowind <perandowind@gmail.com>
2026-02-06 12:28:05 +0900

    12 -g

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class App {
 
     private Scanner sc;
+    int lastId = 0;
 
     public App(Scanner scanner) {
         this.sc = scanner;
@@ -10,20 +11,26 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
-        System.out.println("명령) ");
-        String cmd = sc.nextLine();
 
-        switch (cmd) {
-            case "등록" -> {
-                System.out.print("명언 : ");
-                String saying = sc.nextLine();
-                System.out.print("작가 : ");
-                String author = sc.nextLine();
-                System.out.println("1번 명언이 등록되었습니다.");
+        while(true){
+            System.out.println("명령) ");
+            String cmd = sc.nextLine();
+
+            switch (cmd) {
+                case "등록" -> {
+                    System.out.print("명언 : ");
+                    String saying = sc.nextLine();
+                    System.out.print("작가 : ");
+                    String author = sc.nextLine();
+                    lastId++;
+
+                    System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+                }
+                case "종료" -> {
+                    return;
+                }
             }
-            case "종료" -> {
-                return;
-            }
+
         }
 
     }
