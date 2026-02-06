33714c18875b30b634048c4ab65c1d3a4cc5d588
perandowind <perandowind@gmail.com>
2026-02-06 12:31:32 +0900

    13 -g

---
+++ src/main/java/WiseSaying.java
@@ -0,0 +1,35 @@
+public class WiseSaying {
+    private int id;
+    private String saying;
+    private String author;
+
+    public WiseSaying(int id, String saying, String author) {
+        this.id = id;
+        this.saying = saying;
+        this.author = author;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSaying(String saying) {
+        this.saying = saying;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSaying() {
+        return saying;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
