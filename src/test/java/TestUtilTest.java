cc6fdae1940fdeb657aec5ee437c63afdaf5e686
perandowind <perandowind@gmail.com>
2026-02-06 10:44:32 +0900

    6 -g

--- src/test/java/TestUtilTest.java
+++ src/test/java/TestUtilTest.java
@@ -9,15 +9,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TestUtilTest {
 
     @Test
-    @DisplayName("TestUtil.setOutByteArray()")
+    @DisplayName("TestUtil.genScanner()")
     void t1() {
+        Scanner scanner = TestUtil.genScanner("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        String cmd = scanner.nextLine();
+        String content = scanner.nextLine();
+        String author = scanner.nextLine();
+
+        assertThat(cmd).isEqualTo("등록");
+        assertThat(content).isEqualTo("현재를 사랑하라.");
+        assertThat(author).isEqualTo("작자미상");
+    }
+
+    @Test
+    @DisplayName("TestUtil.setOutByteArray()")
+    void t2() {
         ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutByteArray();
 
         System.out.println("안녕하세요");
 
         String rst = byteArrayOutputStream.toString();
 
-        assertThat(rst).isEqualTo("안녕하세요");
+        assertThat(rst).contains("안녕하세요");
     }
 
 
