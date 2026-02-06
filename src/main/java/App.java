694818a039dd7b680ab1beff5ca174bed642e003
perandowind <perandowind@gmail.com>
2026-02-06 12:18:02 +0900

    10 -b

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,17 +2,29 @@ import java.util.Scanner;
 
 public class App {
 
-    private Scanner scanner;
+    private Scanner sc;
 
     public App(Scanner scanner) {
-        this.scanner = scanner;
+        this.sc = scanner;
     }
 
     public void run() {
         System.out.println("== 명언 앱 ==");
         System.out.println("명령) ");
-        System.out.println("명언 : ");
-        System.out.println("작가 : ");
+        String cmd = sc.nextLine();
+
+        switch (cmd) {
+            case "등록" -> {
+                System.out.print("명언 : ");
+                String saying = sc.nextLine();
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
+            }
+            case "종료" -> {
+                return;
+            }
+        }
+
     }
 
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -8,10 +8,7 @@ public class AppTest {
     @Test
     @DisplayName("'== 명언 앱 ==' 출력")
     void t1() throws Exception {
-        String out = AppTestRunner.run("""
-                종료
-                """
-        );
+        String out = AppTestRunner.run("");
 
         // 굳이 출력해서 볼 필요없다면 생략해도됨
 //        TestUtil.clearSetOutToByteArray(outputStream);
