6aba5a151f9737832d499879dbc1aee2e305954f
perandowind <perandowind@gmail.com>
2026-02-06 11:34:35 +0900

    7 -g

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -10,13 +10,17 @@ public class AppTest {
 
     @Test
     @DisplayName("'== 명언 앱 ==' 출력")
-    void t1() {
+    void t1() throws Exception {
         Scanner sc = TestUtil.genScanner("종료");
 
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ByteArrayOutputStream outputStream = TestUtil.setOutByteArray();
         new App(sc).run();
 
         String out = outputStream.toString();
+
+        // 굳이 출력해서 볼 필요없다면 생략해도됨
+//        TestUtil.clearSetOutToByteArray(outputStream);
+//        System.out.println(out);
 
         assertThat(out).contains("== 명언 앱 ==");
     }
