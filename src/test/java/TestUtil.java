99d1aa55b0b89f32ad8ddd71c0b3735b12d9828b
perandowind <perandowind@gmail.com>
2026-02-06 10:29:43 +0900

    5강 -rg

---
+++ src/test/java/TestUtil.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TestUtil {
+
+//    public static String run(String input) {
+//
+//        // 입력값
+//        Scanner sc = new Scanner(input);
+//
+//        // 명언 프로그램 실행
+//
+//        // 출력값
+//
+//
+//
+//        return "";
+//    }
+
+
+    public static Scanner getScanner(String input){
+        return new Scanner(input);
+    }
+
+}
