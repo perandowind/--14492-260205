2706d53ed3cf32951b6b0cb851346f773ff89c3e
perandowind <perandowind@gmail.com>
2026-02-10 11:37:13 +0900

    41 42

--- src/main/java/com/back/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/back/wiseSaying/entity/WiseSaying.java
@@ -1,12 +1,17 @@
 package com.back.wiseSaying.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Setter
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode //단순 주소값 비교가 아니라 객체 값을 비교한다.
 public class WiseSaying {
     private int id;
     private String saying;
@@ -14,6 +19,23 @@ public class WiseSaying {
 
     public boolean isNew() {
         return id == 0;
+    }
+
+    public Map<String, Object> toMap(){
+        Map<String, Object> map = new LinkedHashMap<>(); //순서 보장
+        map.put("id", id);
+        map.put("saying", saying);
+        map.put("author", author);
+
+        return map;
+    }
+
+    public static WiseSaying fromMap(Map<String, Object> map){
+        int id = (int) map.get("id");
+        String saying = (String) map.get("saying");
+        String author = (String) map.get("author");
+
+        return new WiseSaying(id,saying,author);
     }
 
 
--- src/main/java/com/back/wiseSaying/repository/WiseSayingFileRepository.java
+++ src/main/java/com/back/wiseSaying/repository/WiseSayingFileRepository.java
@@ -1,13 +1,38 @@
 package com.back.wiseSaying.repository;
 
+import com.back.standard.util.Util;
 import com.back.wiseSaying.entity.WiseSaying;
+
+import java.util.Map;
 
 public class WiseSayingFileRepository {
 
-    public void save(WiseSaying wiseSaying) {}
+    private int lastId = 0;
+
+    public WiseSaying save(WiseSaying wiseSaying) {
+        if (wiseSaying.isNew()) {
+
+            wiseSaying.setId(++lastId);
+            Map<String, Object> wiseSayingMap = wiseSaying.toMap();
+            String jsonStr = Util.json.toString(wiseSayingMap);
+            Util.file.set("db/wiseSaying/%d.json".formatted(wiseSaying.getId()), jsonStr);
+
+        }
+        return wiseSaying;
+    }
 
 
     public WiseSaying findByIdOrNull(int id) {
-        return null;
+        String jsonStr = Util.file.get("db/wiseSaying/%d.json".formatted(id), "");
+        if( jsonStr.isBlank()) {
+            return null;
+        }
+
+        Map<String, Object> map = Util.json.toMap(jsonStr);
+        return WiseSaying.fromMap(map);
+
     }
+
+
+
 }
